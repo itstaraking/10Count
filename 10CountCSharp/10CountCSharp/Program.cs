5f09625e4972515f2e5ba7845ce285382d545c5f
itstaraking <fairgameapps@gmail.com>
2017-05-17 23:38:35 -0400

    Add files via upload
    Added C# file

---
+++ 10CountCSharp/10CountCSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10CountCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare maxValue variable to hold max value
+            int maxValue = 10;
+
+            // use for loop for count
+            for(int num = 1; num <= maxValue; num++)
+            {
+                Console.WriteLine(num);
+                Console.ReadLine();
+            }
+        }
+    }
+}
