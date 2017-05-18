42eacffd58b9837869977394c3b7423e1584dd7f
itstaraking <fairgameapps@gmail.com>
2017-05-17 23:57:18 -0400

    Add files via upload
    Added Python File

---
+++ 10countPython/10CountLoop.py
@@ -0,0 +1,15 @@
+# This program uses a for loop to count from 1 to 10
+
+# define the count function
+def count():
+    for num in range(1, 11):
+        print(num)
+
+
+# define the main function
+def main():
+    count();
+
+
+# run main function
+main();
